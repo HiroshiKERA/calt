d9a112757b6ca108b45d7d87d3d90c2e15d004f8
sugarl-sudo <sato.yuta0112@gmail.com>
2025-06-06 20:46:51 +0900

    削除: Dockerfile、Makefile、トレーニング用の設定ファイル、サンプルデータ生成スクリプト、ノートブックファイル、設定ファイル、その他の未使用のスクリプトを削除し、プロジェクトの整理を行いました。また、pyproject.tomlを更新し、依存関係やプロジェクト情報を修正しました。

--- src/calt/__init__.py
+++ src/calt/__init__.py
@@ -4,6 +4,6 @@ from .data_loader.data_loader import data_loader
 from .data_loader.data.data_collator import StandardDataset, StandardDataCollator
 from .data_loader.data.tokenizer import set_tokenizer
 from .data_loader.data.preprocessor import SymbolicToInternalProcessor, IntegerToInternalProcessor
-from .generate.dataset_generator import DatasetGenerator
-from .generate.utils.polynomial_sampler import PolynomialSampler
+# from .generate.dataset_generator import DatasetGenerator
+# from .generate.utils.polynomial_sampler import PolynomialSampler
 from .generate.utils.dataset_writer import DatasetWriter
