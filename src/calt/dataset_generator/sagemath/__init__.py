a345934640c94386b9856aacb392b9f69b4c69b8
shun-arkw <m92fp38@gmail.com>
2025-07-10 01:37:11 +0900

    fix: Add __all__ exports to dataset_generator __init__.py files
    - Add __all__ list to sagemath/__init__.py to export imported classes
    - Add __all__ list to sympy/__init__.py to export imported classes
    - Resolves ruff F401 "imported but unused" errors

--- src/calt/dataset_generator/sagemath/__init__.py
+++ src/calt/dataset_generator/sagemath/__init__.py
@@ -2,3 +2,10 @@ from .dataset_generator import DatasetGenerator
 from .utils.dataset_writer import DatasetWriter
 from .utils.polynomial_sampler import PolynomialSampler
 from .utils.statistics_calculator import BaseStatisticsCalculator
+
+__all__ = [
+    "DatasetGenerator",
+    "DatasetWriter",
+    "PolynomialSampler",
+    "BaseStatisticsCalculator",
+]
