4af5fb4ac00d82dd2062112959ae3ea2512671f1
sugarl-sudo <sato.yuta0112@gmail.com>
2025-11-10 13:43:20 +0900

    feat: update foo function

--- src/calt/trainer/hoge.py
+++ src/calt/trainer/hoge.py
@@ -1,2 +1,14 @@
-def foo():
-    pass
+# src/calt/trainer/hoge.py
+def foo(a: int, b: int) -> int:
+    """
+    foo method
+
+
+    Args:
+            a (int): ---
+            b (int): ---
+
+    Returns:
+            int: ---
+    """
+    return a + b
