ac8e5e7aafab1df1b97c585d724d280c7a902975
Hiroshi Kera <kera.hiroshi@gmail.com>
2026-01-26 23:35:59 +0900

    update for test

--- src/calt/io/validation/__init__.py
+++ src/calt/io/validation/__init__.py
@@ -1,7 +1,14 @@
-from .format_checker import FormatChecker
 from .vocab_validator import validate_dataset_tokens
 
-__all__ = [
-    "FormatChecker",
-    "validate_dataset_tokens",
-]
+# FormatChecker requires SageMath, so import it optionally
+try:
+    from .format_checker import FormatChecker
+    __all__ = [
+        "FormatChecker",
+        "validate_dataset_tokens",
+    ]
+except ImportError:
+    # SageMath not available - FormatChecker will not be available
+    __all__ = [
+        "validate_dataset_tokens",
+    ]
